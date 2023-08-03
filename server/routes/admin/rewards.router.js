5efc8a847878ba888662ae170f2426dde5cd8caf
Luke Gartner <lukegartner4@gmail.com>
2023-08-03 10:07:54 -0500

    joins reward category to rewards to get category name

--- server/routes/admin/rewards.router.js
+++ server/routes/admin/rewards.router.js
@@ -3,8 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  const queryText = `
+        SELECT rewards.*, reward_category
+        FROM "rewards" JOIN "reward_categories" ON rewards.category_id = reward_categories.id;
+    `;
   pool
-    .query(`SELECT * FROM "rewards";`)
+    .query(queryText)
     .then((response) => res.send(response.rows))
     .catch((error) => {
       console.log(error);
