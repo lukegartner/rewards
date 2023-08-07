03511e42805d814e3f888b478cdf6c7e48426290
Luke Gartner <lukegartner4@gmail.com>
2023-08-07 08:40:28 -0500

    styling fix for phone

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -8,8 +8,8 @@ import Button from "@mui/material/Button";
 const LoginPage = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <Container sx={{ width: "90vw", mt: "16rem" }}>
-      <Typography align="center" variant="h1">
+    <Container sx={{ width: "90vw", mt: "33vh" }}>
+      <Typography align="center" variant="h2">
         Welcome
       </Typography>
       <Button
